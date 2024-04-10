f0fe2fa516afca72d71e04b7c8a698bcfa6bb9c6
Seongmin <chr92@naver.com>
2024-04-10 15:36:09 +0900

    chapter_08: Event - Class Component

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,12 +5,13 @@ import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import NotificationList from "./chapter_06/NotificationList";
 import Accommodate from "./chapter_07/Accommodate";
+import ConfirmButton from "./chapter_08/ConfirmButton";
 
 const root = createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <Accommodate />
+    <ConfirmButton />
   </React.StrictMode>
 );
 
