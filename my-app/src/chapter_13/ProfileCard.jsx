7ac0c3476d743f84bb23069cf79c2e477230db95
Seongmin <chr92@naver.com>
2024-04-15 17:12:40 +0900

    chapter_13: Composition

---
+++ my-app/src/chapter_13/ProfileCard.jsx
@@ -0,0 +1,11 @@
+import Card from "./Card";
+
+function ProfileCard(props) {
+  return (
+    <Card title="Seongmin" backgroundColor="#4ea04e">
+      <p>안녕하세요.</p>
+      <p>리액트 개발 중입니다.</p>
+    </Card>
+  );
+}
+export default ProfileCard;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -10,12 +10,13 @@ import LandingPage from "./chapter_09/LandingPage";
 import AttendanceBooks from "./chapter_10/AttendanceBook";
 import SignUp from "./chapter_11/SignUp";
 import Calculator from "./chapter_12/Calculator";
+import ProfileCard from "./chapter_13/ProfileCard";
 
 const root = createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <Calculator />
+    <ProfileCard />
   </React.StrictMode>
 );
 
