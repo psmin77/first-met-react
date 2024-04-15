377af477c6abac98eee2f8ce20ecd0bd02c4b11c
Seongmin <chr92@naver.com>
2024-04-15 16:40:06 +0900

    chapter_12: Lifting State Up

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -9,12 +9,13 @@ import ConfirmButton from "./chapter_08/ConfirmButton";
 import LandingPage from "./chapter_09/LandingPage";
 import AttendanceBooks from "./chapter_10/AttendanceBook";
 import SignUp from "./chapter_11/SignUp";
+import Calculator from "./chapter_12/Calculator";
 
 const root = createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <SignUp />
+    <Calculator />
   </React.StrictMode>
 );
 
